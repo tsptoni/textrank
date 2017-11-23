7767d210717761ba501a3a12685751dd088bba6b
Antonio Sanchez Pineda <tsptoni@gmail.com>
2017-11-23 12:41:21 +0100

    renamed package to textrank3

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 # Instructions from http://peterdowns.com/posts/first-time-with-pypi.html
 # and https://github.com/pypa/sampleproject/blob/master/setup.py
 setup(
-    name = 'summa',
-    packages = ['summa', 'summa.preprocessing'],
+    name = 'textrank3',
+    packages = ['textrank3', 'textrank3.preprocessing'],
     package_data = {
         'summa': ['README', 'LICENSE']
     },
-    version = '0.0.7',
+    version = '0.0.4',
     description = 'A text summarization and keyword extraction package',
-    author = 'Federico Barrios, Federico Lopez',
+    author = 'Federico Barrios, Federico Lopez, Antonio Sanchez Pineda',
     author_email = 'summanlp@gmail.com',
-    url = 'http://summa.fbarrios.com.ar/',
-    download_url = 'https://github.com/summanlp/textrank/tarball/v0.0.2',
+    url = 'https://github.com/tsptoni/textrank3',
+    download_url = 'https://github.com/tsptoni/textrank3',
     keywords = ['summa', 'nlp', 'summarization', "NLP", "natural language processing", "automatic summarization",
         "keywords", "summary", "textrank", "pagerank"],
     classifiers = [
@@ -32,7 +32,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3'
     ],
     long_description = open('README').read()
 )
--- textrank3/__init__.py
+++ textrank3/__init__.py
@@ -1,3 +1,3 @@
 
-from summa import commons, export, graph, keywords, pagerank_weighted, \
+from textrank3 import commons, export, graph, keywords, pagerank_weighted, \
                   summarizer, syntactic_unit, textrank, textrank_runtime_error
